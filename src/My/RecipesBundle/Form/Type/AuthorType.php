43be313a02404add3b003b216f43953b5bfbf0c9
carlescliment <carlescliment@gmail.com>
2013-11-26 15:53:10 +0100

    Validation

--- src/My/RecipesBundle/Form/Type/AuthorType.php
+++ src/My/RecipesBundle/Form/Type/AuthorType.php
@@ -1,24 +1,35 @@
 <?php
 
-// src/My/RecipesBundle/Form/Type/RecipeType.php
+// src/My/RecipesBundle/Form/Type/AuthorType.php
 namespace My\RecipesBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
 
 class AuthorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->setMethod('PUT')
             ->add('name', 'text')
-            ->add('surname', 'text')
+            ->add('surname', 'text', array('required' => false))
             ->add('save', 'submit');
     }
 
     public function getName()
     {
-        return 'recipe';
+        return 'author';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            // a unique key to help generate the secret token
+            'intention'       => 'author_item',
+        ));
     }
 }
